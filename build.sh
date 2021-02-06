14da00c971e280d05ff5494a6f449b93416d47e8
kevall474 <kevall474@tuta.io>
2021-02-06 18:16:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,12 @@ cd xf86-video-intel-git && makepkg -si
 
 cd ..
 
+# make a copy of every pkg in pkg/ dir
+
+mkdir pkg
+cp -v */*.pkg.tar.zst pkg/
+
+# clean build dir
+
+rm -rf */src/
+rm -rf */pkg/
