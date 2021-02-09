f25c08412785b7291ffaf9a629841d030f6f0a8c
Kevin M <kevall474@tuta.io>
2021-02-09 03:32:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/bash
 
-cd xf86-video-amdgpu-git && makepkg -si
+cd xf86-video-amdgpu-git && env _compiler=2 makepkg -si
 
 cd ..
 
-cd xf86-video-intel-git && makepkg -si
+cd xf86-video-intel-git && _compiler=2 makepkg -si
 
 cd ..
 
-cd makedepend-git && makepkg -si
+cd xf86-video-nouveau-git && _compiler=2 makepkg -si
 
 cd ..
 
-cd xorg-util-macros-git && makepkg -si
+cd makedepend-git && _compiler=2 makepkg -si
+
+cd ..
+
+cd xorg-util-macros-git && _compiler=2 makepkg -si
 
 cd ..
 
