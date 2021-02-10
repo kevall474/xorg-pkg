0e014e6bf13bcd3e5a74366c2c83496a85de5eaa
kevall474 <kevall474@tuta.io>
2021-02-09 23:52:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/bash
 
-cd xf86-video-amdgpu-git && env _compiler=2 makepkg -si
+cd xf86-video-amdgpu-git && env _compiler=2 makepkg -si && cd ..
 
-cd ..
+cd xf86-video-intel-git && _compiler=2 makepkg -si && cd ..
 
-cd xf86-video-intel-git && _compiler=2 makepkg -si
+cd xf86-video-nouveau-git && _compiler=2 makepkg -si && cd ..
 
-cd ..
+cd makedepend-git && _compiler=2 makepkg -si && cd ..
 
-cd xf86-video-nouveau-git && _compiler=2 makepkg -si
-
-cd ..
-
-cd makedepend-git && _compiler=2 makepkg -si
-
-cd ..
-
-cd xorg-util-macros-git && _compiler=2 makepkg -si
-
-cd ..
+cd xorg-util-macros-git && _compiler=2 makepkg -si && cd ..
 
 # make a copy of every pkg in pkg/ dir
 
 mkdir pkg
+
 cp -v */*.pkg.tar.zst pkg/
 
 # clean build dir
