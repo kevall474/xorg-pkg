46b7a11c0e265ccd3e3eb3ff8255f8bffd76eb0b
kevall474 <kevall474@tuta.io>
2021-02-09 23:55:40 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+#_                   _ _ _  _ _____ _  _
+#| | _______   ____ _| | | || |___  | || |
+#| |/ / _ \ \ / / _` | | | || |_ / /| || |_
+#|   <  __/\ V / (_| | | |__   _/ / |__   _|
+#|_|\_\___| \_/ \__,_|_|_|  |_|/_/     |_|
+
 cd xf86-video-amdgpu-git && env _compiler=2 makepkg -si && cd ..
 
 cd xf86-video-intel-git && _compiler=2 makepkg -si && cd ..
