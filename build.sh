736e34f0da3a3b0d40a407dd3202a9249a46edae
kevall474 <kevall474@tuta.io>
2021-02-12 20:46:52 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,11 +16,11 @@ cd xf86-video-nouveau-git && _compiler=2 makepkg -si && cd ..
 
 cd xorg-util-macros-git && _compiler=2 makepkg -si && cd ..
 
-# make a copy of every pkg in package/ dir
+# make a copy of every pkg in package-$(date -I)/ dir
 
-mkdir package
+mkdir package-$(date -I)
 
-cp -v */*.pkg.tar.zst package/
+cp -v */*.pkg.tar.zst package-$(date -I)/
 
 # clean build dir
 
